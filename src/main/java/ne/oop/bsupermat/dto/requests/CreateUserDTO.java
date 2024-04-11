f897b48c54ba4dc6553c0632b710f88c90282731
Dorcus-1 <mugishadorcus0@gmail.com>
2024-04-12 01:20:41 +0200

    [ADD] Create UserSercivesImplementation

---
+++ src/main/java/ne/oop/bsupermat/dto/requests/CreateUserDTO.java
@@ -0,0 +1,17 @@
+package ne.oop.bsupermat.dto.requests;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class CreateUserDTO {
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String email;
+    private String password;
+
+}
---
+++ src/main/java/ne/oop/bsupermat/serviceImpls/UserServiceImpl.java
@@ -0,0 +1,55 @@
+package ne.oop.bsupermat.serviceImpls;
+
+import lombok.RequiredArgsConstructor;
+import ne.oop.bsupermat.dto.requests.CreateUserDTO;
+import ne.oop.bsupermat.exceptions.BadRequestException;
+import ne.oop.bsupermat.model.User;
+import ne.oop.bsupermat.repositories.UserRepository;
+import ne.oop.bsupermat.services.UserService;
+import ne.oop.bsupermat.utils.Hash;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUserById(String id) {
+        return null;
+    }
+
+    @Override
+    public User saveUser(CreateUserDTO createUserDTO) {
+        try {
+           Optional <User> user=userRepository.findByEmail(createUserDTO.getEmail());
+           if (user.isPresent()){
+             throw new BadRequestException("Email already exist");
+           }
+           User user1= new User();
+           user1.setFirstName(createUserDTO.getFirstname());
+           user1.setLastName(createUserDTO.getLastname());
+           user1.setEmail(createUserDTO.getEmail());
+           user1.setPassword(Hash.hashPassword(createUserDTO.getPassword()));
+           return userRepository.save(user1);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteUser(String id) {
+
+    }
+}
